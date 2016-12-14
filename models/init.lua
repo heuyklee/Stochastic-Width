de0f4ba896206c2dc240cfa5fa011a11ba453ca1
giyobe <heuyklee@ajou.ac.kr>
2016-12-14 13:49:24 +0900

    getConv 병합

--- models/init.lua
+++ models/init.lua
@@ -23,6 +23,9 @@ function M.setup(opt, checkpoint)
       assert(paths.filep(modelPath), 'Saved model not found: ' .. modelPath)
       print('=> Resuming model from ' .. modelPath)
       model = torch.load(modelPath):cuda()
+require '../SpatialConvolution2'
+require '../SpatialBatchNormalization2'
+model:initConvTbl()
    elseif opt.retrain ~= 'none' then
       assert(paths.filep(opt.retrain), 'File not found: ' .. opt.retrain)
       print('Loading model from file: ' .. opt.retrain)
